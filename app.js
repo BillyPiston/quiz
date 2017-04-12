8545f4303fc9a95487fc7d4cdbe7598f90add561
BillyPiston <oklahoma13state@gmail.com>
2017-04-12 09:52:14 +0300

    add of js code and css

--- app.js
+++ app.js
@@ -1,3 +1,66 @@
-/**
- * Created by Redkliff on 11.04.2017.
- */
+function populate() {
+    if (quiz.isEnded()){
+        showScores();
+    }
+    else {
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        var choices = quiz.getQuestionIndex().choices;
+        for (var i = 0; i< choices.length; i++){
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
+
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function () {
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Вопрос " + currentQuestionNumber + " из " + quiz.questions.length;
+}
+
+function showScores() {
+    var gameOverHtml = "<h1>Result</h1>";
+        gameOverHtml += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+        var element = document.getElementById("quiz");
+        element.innerHTML = gameOverHtml;
+};
+
+var questions = [
+    new Question("Какой тег отвечает за стилизацию элементов в HTML?", ["body", "html", "style", "head"], "style"),
+    new Question("Какой тег отвечает за стилизацию элементов в HTML?", ["body", "html", "style", "head"], "style"),
+    new Question("Какой тег отвечает за стилизацию элементов в HTML?", ["body", "html", "style", "head"], "style"),
+    new Question("Какой тег отвечает за стилизацию элементов в HTML?", ["body", "html", "style", "head"], "style"),
+    new Question("Какой тег отвечает за стилизацию элементов в HTML?", ["body", "html", "style", "head"], "style"),
+    new Question("Какой тег отвечает за стилизацию элементов в HTML?", ["body", "html", "style", "head"], "style"),
+    new Question("Какой тег отвечает за стилизацию элементов в HTML?", ["body", "html", "style", "head"], "style"),
+    new Question("Какой тег отвечает за стилизацию элементов в HTML?", ["body", "html", "style", "head"], "style"),
+    new Question("Какой тег отвечает за стилизацию элементов в HTML?", ["body", "html", "style", "head"], "style"),
+    new Question("Какой тег отвечает за стилизацию элементов в HTML?", ["body", "html", "style", "head"], "style"),
+    new Question("Какой тег отвечает за стилизацию элементов в HTML?", ["body", "html", "style", "head"], "style"),
+    new Question("Какой тег отвечает за стилизацию элементов в HTML?", ["body", "html", "style", "head"], "style"),
+    new Question("Какой тег отвечает за стилизацию элементов в HTML?", ["body", "html", "style", "head"], "style"),
+    new Question("Какой тег отвечает за стилизацию элементов в HTML?", ["body", "html", "style", "head"], "style"),
+    new Question("Какой тег отвечает за стилизацию элементов в HTML?", ["body", "html", "style", "head"], "style"),
+    new Question("Какой тег отвечает за стилизацию элементов в HTML?", ["body", "html", "style", "head"], "style"),
+    new Question("Какой тег отвечает за стилизацию элементов в HTML?", ["body", "html", "style", "head"], "style"),
+    new Question("Какой тег отвечает за стилизацию элементов в HTML?", ["body", "html", "style", "head"], "style"),
+    new Question("Какой тег отвечает за стилизацию элементов в HTML?", ["body", "html", "style", "head"], "style"),
+    new Question("Какой тег отвечает за стилизацию элементов в HTML?", ["body", "html", "style", "head"], "style")
+];
+
+var quiz = new Quiz(questions);
+
+populate();
